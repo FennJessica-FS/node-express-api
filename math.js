0a3963662ec376db33ff9090b3297b9c076ca8a5
FennJessica-FS <JLFenn@student.fullsail.edu>
2026-01-09 14:36:18 -0500

    Add math module and Jest tests

---
+++ math.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function sqrt(n) {
+  return Math.sqrt(n);
+}
+
+function max(a, b) {
+  return Math.max(a, b);
+}
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide,
+  sqrt,
+  max,
+};
